021ec8f5a262828ae9155bbc2f58dd3346a0b7e0
Mastej, Matt <MattMastej@quickenloans.com>
2019-09-03 15:51:54 -0400

    testing form

---
+++ src/js/cms-preview-templates/formtest.js
@@ -0,0 +1,35 @@
+import React from "react";
+import format from "date-fns/format";
+
+import Jumbotron from "./components/jumbotron";
+
+export default class PostPreview extends React.Component {
+  render() {
+    const {entry, getAsset} = this.props;
+    let image = getAsset(entry.getIn(["data", "image"]));
+
+    // Bit of a nasty hack to make relative paths work as expected as a background image here
+    if (image && !image.fileObj) {
+        image = window.parent.location.protocol + "//" + window.parent.location.host + image;
+    }
+
+    return <div>
+        <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
+
+        <div class="container">
+            <form name="formtest-contact" netlify>
+                <p>
+                    <label>Name <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Email <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        </div>
+
+    </div>
+  }
+}
